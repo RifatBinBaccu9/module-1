ef146f121372d8a80add94fefac0870222c2102b
rifatbinbaccu9 <rifatbinbaccu@gmail.com>
2025-06-11 21:58:51 +0600

    first commit

---
+++ bin/data.dart
@@ -0,0 +1,14 @@
+main(){
+  String name = "55";
+  print(name);
+
+  int number = 44;
+  double convert = double.parse(name);
+  print(convert);
+
+  bool data = true;
+  print(data);
+
+  double height = 4.5;
+  print(height.round());
+}
