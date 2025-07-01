0647be840f690e42aa96bce450de90ce78fb08a9
rifatbinbaccu9 <rifatbinbaccu@gmail.com>
2025-07-02 00:39:47 +0600

    dart

--- bin/namedFunction.dart
+++ bin/namedFunction.dart
@@ -2,7 +2,7 @@ void main(){
   findVolume(10,  height: 30 , breadth: 20);
 }
 
-void findVolume( int length, { int? breadth, int? height}){
+void findVolume( int length, { required int? breadth, int? height=10, int data=0}){
   print("length1 $length");
   print("length2 $breadth");
   print("length3 $height");
